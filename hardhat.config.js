acff6e80b610336f18b74775262aa6100dd18ae6
Tekin Zeiadin <tekinzeiadin@MacBook-Air-de-Tekin.local>
2024-01-28 12:27:29 +0100

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,45 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("dotenv").config()
+require("@nomiclabs/hardhat-ethers")
+require("ethers")
+require("chai")
+require("hardhat-gas-reporter")
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
+    },
+}
